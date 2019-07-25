04a280635be69764d0cabd1e8e40f3967339a4c2
Kwang Wei Tan <s10186878@connect.np.edu.sg>
2019-07-25 21:37:46 +0800

    updated folder names

---
+++ app/src/main/java/com/jiak/app/Main3Activity.java
@@ -0,0 +1,46 @@
+package com.jiak.app;
+
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+public class Main3Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fragment);
+
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
+        bottomNav.setOnNavigationItemSelectedListener(navlistener);
+
+    }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navlistener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    Fragment selectedFragment = null;
+
+                    switch(item.getItemId()){
+                        case R.id.nav_Main:
+                            selectedFragment = new MainFragment();
+                            break;
+                        case R.id.nav_Side:
+                            selectedFragment = new SideFragment();
+                            break;
+                        case R.id.nav_Drinks:
+                            selectedFragment = new DrinkFragment();
+                            break;
+                    }
+
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                            selectedFragment).commit();
+
+                    return true;
+                }
+            };
+}
