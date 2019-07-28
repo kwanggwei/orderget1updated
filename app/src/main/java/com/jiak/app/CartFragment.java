8571b2798b964ad08ab5fc0ad043d978e51cfb58
Kwang Wei Tan <s10186878@connect.np.edu.sg>
2019-07-28 20:46:56 +0800

    notification of adding

--- app/src/main/java/com/jiak/app/CartFragment.java
+++ app/src/main/java/com/jiak/app/CartFragment.java
@@ -1,5 +1,7 @@
 package com.jiak.app;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
@@ -8,8 +10,10 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.io.File;
 
@@ -19,7 +23,24 @@ public class CartFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_cart, container, false);
+        View v =  inflater.inflate(R.layout.fragment_cart, container, false);
+        Button add = (Button) v.findViewById(R.id.addbtn);
+        add.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                AlertDialog.Builder alert = new AlertDialog.Builder(getContext());
+                alert.setMessage("Order has been added");
+                alert.setNeutralButton("Ok", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(getContext(),"Added", Toast.LENGTH_SHORT).show();
+                    }
+                });
+                alert.show();
+            }
+        });
+
+        return v;
     }
 
     public void onCreate(Bundle savedInstanceState){
@@ -42,4 +63,6 @@ public class CartFragment extends Fragment {
         Name.setText(getName);
         Image.setImageURI(Uri.fromFile(file));
     }
+
+
 }
